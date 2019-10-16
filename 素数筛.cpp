f925aa003e95eba223c861966246903abc3c4e35
famouscat8 <3142362556@qq.com>
2019-10-16 20:57:33 +0800

    添加项目文件。

---
+++ 素数筛.cpp
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int* pnum = NULL;
+	int n = 0;
+	scanf_s("%d",&n);
+
+	pnum= (int*)calloc(n+100, sizeof(int));
+	if (pnum == NULL)return -1;
+	for (int j = 0; j < (n+1); j++) {
+		pnum[j] = j ;
+	}
+	
+
+	
+
+	for (int i = 3; i <= sqrt(n); i += 2) {
+		if (*pnum) {
+			for (int j = i + i; j <= n; j += i) {
+				pnum[j] = 0;
+			}
+		}
+		pnum++;
+	}
+
+
+	for (int z = 0; z < n + 1; z++) {
+		printf("%d\n", *pnum);
+		pnum++;
+		//printf("%d", pnum);
+	}
+
+
+	return 0;
+}
